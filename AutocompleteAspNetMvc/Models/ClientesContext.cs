8ff81ff41097e12f9c67aafef420bb297d2b0031
miilleramaral <miilleramaral@gmail.com>
2018-06-28 19:50:12 -0300

    Add project files.

---
+++ AutocompleteAspNetMvc/Models/ClientesContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace AutocompleteAspNetMvc.Models
+{
+    public class ClientesContext : DbContext
+    {
+        public ClientesContext():base("DbAutocomplete")
+        {
+            Database.Log = instrucao => System.Diagnostics.Debug.WriteLine(instrucao);
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
+    }
+}
